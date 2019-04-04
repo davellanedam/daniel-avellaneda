2e4b89b7fe499ab54cea2ac34783188a1669b911
Daniel Avellaneda <davellanedam@gmail.com>
2019-04-04 16:36:59 -0500

    logo

--- src/plugins/common.js
+++ src/plugins/common.js
@@ -2,7 +2,9 @@ import Vue from 'vue'
 import Heading from '@/components/common/Heading'
 import Subheading from '@/components/common/Subheading'
 import ContentText from '@/components/common/ContentText'
+import Logo from '@/components/common/Logo'
 
 Vue.component('Heading', Heading)
 Vue.component('Subheading', Subheading)
 Vue.component('ContentText', ContentText)
+Vue.component('Logo', Logo)
