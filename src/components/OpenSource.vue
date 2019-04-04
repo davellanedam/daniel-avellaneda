b5c60c74a20d3f96b704d8d312e7925b75448d55
Daniel Avellaneda <davellanedam@gmail.com>
2019-04-04 16:36:24 -0500

    format

--- src/components/OpenSource.vue
+++ src/components/OpenSource.vue
@@ -2,22 +2,22 @@
   <section id="open-source" class="hide-overflow">
     <v-layout>
       <v-flex xs12 v-show="$vuetify.breakpoint.smAndDown">
-        <v-img :src="require('@/assets/open-source.jpg')" height="100%"/>
+        <v-img :src="require('@/assets/open-source.jpg')" height="100%" />
       </v-flex>
     </v-layout>
     <v-layout light-green lighten-1 white--text wrap>
       <v-flex md6>
-        <v-img :src="require('@/assets/open-source.jpg')" height="100%"/>
+        <v-img :src="require('@/assets/open-source.jpg')" height="100%" />
       </v-flex>
       <v-flex xs12 md6>
         <v-layout align-center justify-center fill-height wrap>
           <v-flex xs12 pa-5>
-            <Heading v-html="$t('open_source.TITLE')"/>
-            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_1')"/>
-            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_2')"/>
-            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_3')"/>
-            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_4')"/>
-            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_5')"/>
+            <Heading v-html="$t('open_source.TITLE')" />
+            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_1')" />
+            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_2')" />
+            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_3')" />
+            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_4')" />
+            <ContentText class="mb-5" v-html="$t('open_source.PARAGRAPH_5')" />
           </v-flex>
         </v-layout>
       </v-flex>
