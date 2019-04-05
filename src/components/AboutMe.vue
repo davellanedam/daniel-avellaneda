fcd25a3ef543f423699c68cc3dad7339074a2b8e
Daniel Avellaneda <davellanedam@gmail.com>
2019-04-05 11:51:22 -0500

    less skills listed

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -49,43 +49,13 @@ export default {
         name: 'NodeJS'
       },
       {
-        name: 'ExpressJS'
-      },
-      {
-        name: 'JWT'
-      },
-      {
-        name: 'ES6'
-      },
-      {
         name: 'VueJS'
-      },
-      {
-        name: 'Vuex'
-      },
-      {
-        name: 'Web Development'
-      },
-      {
-        name: 'Testing'
-      },
-      {
-        name: 'REST API'
       },
       {
         name: 'PhalconPHP'
       },
       {
         name: 'PHP'
-      },
-      {
-        name: 'Git'
-      },
-      {
-        name: 'LAMP'
-      },
-      {
-        name: 'GruntJS'
       },
       {
         name: 'MongoDB'
@@ -95,84 +65,6 @@ export default {
       },
       {
         name: 'Redis'
-      },
-      {
-        name: 'jQuery'
-      },
-      {
-        name: 'AJAX'
-      },
-      {
-        name: 'JSON'
-      },
-      {
-        name: 'MQTT'
-      },
-      {
-        name: 'Microservices'
-      },
-      {
-        name: 'Team Leadership'
-      },
-      {
-        name: 'Docker'
-      },
-      {
-        name: 'Web Services API'
-      },
-      {
-        name: 'AWS'
-      },
-      {
-        name: 'Auth0'
-      },
-      {
-        name: 'macOS'
-      },
-      {
-        name: 'Linux'
-      },
-      {
-        name: 'Web Design'
-      },
-      {
-        name: 'E-commerce'
-      },
-      {
-        name: 'Bash'
-      },
-      {
-        name: 'SEO'
-      },
-      {
-        name: 'Bootstrap'
-      },
-      {
-        name: 'SVN'
-      },
-      {
-        name: 'LESS'
-      },
-      {
-        name: 'Back-end web development'
-      },
-      {
-        name: 'Networking'
-      },
-      {
-        name: 'Databases'
-      },
-      {
-        name: 'Web Applications'
-      },
-      {
-        name: 'RESTful WebServices'
-      },
-      {
-        name: 'Microsoft Office'
-      },
-      {
-        name: 'Adobe Photoshop'
       }
     ]
   })
