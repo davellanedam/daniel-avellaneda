41c5bc0c217fe4f3a70ede88ad744a3be3349c7c
Daniel Avellaneda <davellanedam@gmail.com>
2019-04-05 10:11:31 -0500

    auto detect browser language

--- vue.config.js
+++ vue.config.js
@@ -2,30 +2,8 @@ module.exports = {
   productionSourceMap: false,
   pluginOptions: {
     i18n: {
-      locale: 'en',
-      fallbackLocale: 'en',
       localeDir: 'locales',
       enableInSFC: true
     }
   }
-  // chainWebpack: config => {
-  //   config.plugin('preload').tap(options => {
-  //     options[0].as = entry => {
-  //       if (/\.css$/.test(entry)) {
-  //         return 'style'
-  //       }
-  //       if (/\.woff$/.test(entry)) {
-  //         return 'font'
-  //       }
-  //       if (/\.png$/.test(entry)) {
-  //         return 'image'
-  //       }
-  //       return 'script'
-  //     }
-  //     options[0].include = 'allAssets'
-  //     // options[0].fileWhitelist: [/\.files/, /\.to/, /\.include/]
-  //     options[0].fileBlacklist = [/\.js/, /\.jpg/, /\.png/, /\.css/]
-  //     return options
-  //   })
-  // }
 }
