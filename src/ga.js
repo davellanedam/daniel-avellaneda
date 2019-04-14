006834e7bc6d44ac9193c386f77afeaff2cfc4a2
Daniel Avellaneda <davellanedam@gmail.com>
2019-04-14 18:03:11 -0500

    export default google analytics

--- src/ga.js
+++ src/ga.js
@@ -2,8 +2,12 @@ import Vue from 'vue'
 import VueAnalytics from 'vue-analytics'
 
 // Use Google Analytics only in production mode
-if (process.env.NODE_ENV === 'production') {
-  Vue.use(VueAnalytics, {
-    id: 'UA-137710319-1'
-  })
+const ga = () => {
+  if (process.env.NODE_ENV === 'production') {
+    Vue.use(VueAnalytics, {
+      id: 'UA-137710319-1'
+    })
+  }
 }
+
+export default ga
